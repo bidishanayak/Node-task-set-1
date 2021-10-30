5f5d51f3b648611be0722b0851035f762f4587fd
bidishanayak <93401049+bidishanayak@users.noreply.github.com>
2021-10-30 13:00:23 +0530

    Add files via upload

---
+++ database.js
@@ -0,0 +1,3 @@
+const uri = "mongodb+srv://bidishanayak:pass12345@cluster0.napdc.gcp.mongodb.net/ecomm?retryWrites=true&w=majority";
+
+module.exports = uri;
---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.port || 5000;
+
+// routers!
+const productRouter = require('./routes/product.js');
+
+// get the database uri!
+const uri = require('./database');
+
+// middleware!
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+// Mongoose connection!
+mongoose.connect(uri,() => {
+    console.log('Connected to the db!');
+})
+
+// routes!
+app.use('/product',productRouter);
+
+// Listening at port 5000!
+app.listen(PORT,() => {
+    console.log(`Server running at port: ${PORT}`);
+}) 
